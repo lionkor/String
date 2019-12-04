93ba5bc30997bc99ae6989155e7ba608fc6f0783
Lion Kortlepel <development@kortlepel.com>
2019-12-04 10:57:44 +0100

    Reformat all sources

--- Exceptions.h
+++ Exceptions.h
@@ -6,38 +6,39 @@
 class StringBuilder_InvalidAppendAfterBuild : public std::exception
 {
 public:
-    virtual const char* what() const noexcept
-    {
-        return "StringBuilder: Append after build not allowed!";
-    }
+	virtual const char* what() const noexcept
+	{
+		return "StringBuilder: Append after build not allowed!";
+	}
 };
 
 class StringBuilder_DoubleBuildNotAllowed : public std::exception
 {
 public:
-    virtual const char* what() const noexcept
-    {
-        return "StringBuilder: Can only build once - StringBuilder has already transferred ownership!";
-    }
+	virtual const char* what() const noexcept
+	{
+		return "StringBuilder: Can only build once - StringBuilder has already "
+			   "transferred ownership!";
+	}
 };
 
 class FormatEncodingError : public std::exception
 {
 public:
-    virtual const char* what() const noexcept
-    {
-        // Helping the user guess what happened.
-        return "Format encoding failed. Possibly invalid format passed or wrong type?";
-    }
+	virtual const char* what() const noexcept
+	{
+		// Helping the user guess what happened.
+		return "Format encoding failed. Possibly invalid format passed or wrong type?";
+	}
 };
 
 class FormatWriteFault : public std::exception
 {
 public:
-    virtual const char* what() const noexcept
-    {
-        return "Writing formatted string was not successful. (vsnprintf returned >=n)";
-    }
+	virtual const char* what() const noexcept
+	{
+		return "Writing formatted string was not successful. (vsnprintf returned >=n)";
+	}
 };
 
 #endif // EXCEPTIONS_H
--- StringView.cpp
+++ StringView.cpp
@@ -2,4 +2,6 @@
 #include "String.h"
 #include <cstring>
 
-StringView::StringView(const String& str) : m_chars(str.c_str()), m_size(str.size()) {}
+StringView::StringView(const String& str) : m_chars(str.c_str()), m_size(str.size())
+{
+}
