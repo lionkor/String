bcc5ca8d43df91e433b43d24169c4787059772d5
Lion Kortlepel <development@kortlepel.com>
2020-02-01 15:32:49 +0100

    Revert changes to StringView since they break the build

--- include/StringView.h
+++ include/StringView.h
@@ -15,44 +15,38 @@ static inline constexpr std::size_t string_length(const char* cstr) {
     return length;
 }
 
-template<typename _T>
+template <typename _T>
 static inline constexpr _T min(_T&& left, _T&& right) {
     return left < right ? left : right;
 }
 
 /// View to a null-terminated string.
-class StringView
-{
-public:
-    constexpr StringView()
-        : m_chars(nullptr), m_size(0) {}
+class StringView {
+   public:
+    constexpr StringView() : m_chars(nullptr), m_size(0) {}
     constexpr StringView(const char* cstr)
         : m_chars(cstr), m_size(string_length(cstr)) {}
     constexpr StringView(const StringView& other)
-        : m_chars(other.m_chars), m_size(other.m_size) {
-    }
+        : m_chars(other.m_chars), m_size(other.m_size) {}
     constexpr StringView(const char* begin, const char* end);
     StringView(const class String&);
 
-private:
-    /// Caution: The given const char* still has to be null-terminated exactly at the
-    /// given size, otherwise behaviour of StringView isn't defined.
+   private:
+    /// Caution: The given const char* still has to be null-terminated exactly
+    /// at the given size, otherwise behaviour of StringView isn't defined.
     constexpr StringView(const char* cs, std::size_t sz)
         : m_chars(cs), m_size(sz) {}
 
-public:
-    inline constexpr bool        is_valid() const { return m_chars != nullptr; }
-    inline constexpr bool        is_empty() const { return m_size == 0 || m_chars[0] == 0; }
-    inline constexpr size_t      size() const { return m_size; }
+   public:
+    inline constexpr bool is_valid() const { return m_chars != nullptr; }
+    inline constexpr bool is_empty() const {
+        return m_size == 0 || m_chars[0] == 0;
+    }
+    inline constexpr size_t size() const { return m_size; }
     inline constexpr const char* chars() const { return m_chars; }
     inline constexpr const char* c_str() const { return m_chars; }
 
-    constexpr operator const char*() const {
-        return chars();
-    }
-
-    constexpr const char&
-    operator[](std::size_t i) const { return m_chars[i]; }
+    constexpr const char& operator[](std::size_t i) const { return m_chars[i]; }
 
     constexpr bool operator==(const char* other) const {
         return equals(other, strlen(other));
@@ -62,16 +56,20 @@ public:
         return equals(other.m_chars, other.m_size);
     }
 
-    template<typename _StringType>
+    template <typename _StringType>
     constexpr bool operator==(const _StringType& other) const {
         return equals(other.c_str(), other.size());
     }
 
-    constexpr bool operator!=(const char* other) const { return !(*this == other); }
+    constexpr bool operator!=(const char* other) const {
+        return !(*this == other);
+    }
 
-    constexpr bool operator!=(const StringView& other) const { return !(*this == other); }
+    constexpr bool operator!=(const StringView& other) const {
+        return !(*this == other);
+    }
 
-    template<typename _StringType>
+    template <typename _StringType>
     constexpr bool operator!=(const _StringType& other) const {
         return !(*this == other);
     }
@@ -83,39 +81,39 @@ public:
             return os << "";
     }
 
-    friend constexpr StringView operator""_sv(const char* cstr, std::size_t size);
+    friend constexpr StringView operator""_sv(const char* cstr,
+                                              std::size_t size);
 
-private:
+   private:
     inline constexpr bool equals(const char* str, std::size_t size) const {
         // if one is nullptr and the other isn't, they aren't equal
-        if ((m_chars == nullptr && str != nullptr) || (m_chars != nullptr && str == nullptr))
+        if ((m_chars == nullptr && str != nullptr) ||
+            (m_chars != nullptr && str == nullptr))
             return false;
         // if they are both nullptr we consider them equal
-        if (m_chars == nullptr && str == nullptr)
-            return true;
-        // if other is larger than our size we aren't equal. If we didn't have this check,
-        // strncmp would return 0 (equal) for example for:
-        //  strncmp("Hello", "Hello, World", strlen("Hello")), since "Hello" would have
-        //  size 5, and they equal that far.
-        // Similarly, if it's shorter, they aren't equal. If we're going to check length
-        // we might as well check this as well and potentially save ourselves a strncmp.
-        if (size != m_size)
-            return false;
-        // Because of earlier checks we can now assume same size and that both are not
-        // nullptr.
+        if (m_chars == nullptr && str == nullptr) return true;
+        // if other is larger than our size we aren't equal. If we didn't have
+        // this check, strncmp would return 0 (equal) for example for:
+        //  strncmp("Hello", "Hello, World", strlen("Hello")), since "Hello"
+        //  would have size 5, and they equal that far.
+        // Similarly, if it's shorter, they aren't equal. If we're going to
+        // check length we might as well check this as well and potentially save
+        // ourselves a strncmp.
+        if (size != m_size) return false;
+        // Because of earlier checks we can now assume same size and that both
+        // are not nullptr.
         return strncmp(m_chars, str, m_size) == 0;
     }
 
-    const char*       m_chars;
+    const char* m_chars;
     const std::size_t m_size;
 };
 
 constexpr StringView::StringView(const char* begin, const char* end)
-    : m_chars(begin), m_size(end - begin) {
-}
+    : m_chars(begin), m_size(end - begin) {}
 
 constexpr StringView operator""_sv(const char* cstr, std::size_t size) {
     return StringView(cstr, size);
 }
 
-#endif // STRINGVIEW_H
+#endif  // STRINGVIEW_H
